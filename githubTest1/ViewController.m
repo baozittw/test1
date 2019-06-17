2424e5910c12f982a4abf42f07e21e2391ce2291
qixiao <qixiao@qixiaodeMacBook-Air.local>
2019-06-17 14:36:49 +0800

    sd

--- githubTest1/ViewController.m
+++ githubTest1/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"我是11111");
+    NSLog(@"我是hahahaha ");
 }
 
 
