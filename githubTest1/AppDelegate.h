fda8bb1cc785fb988fb6de3120f52e82a37d3c6a
qixiao <qixiao@qixiaodemacbook-air.local>
2019-06-17 14:10:46 +0800

    Initial Commit

---
+++ githubTest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest1
+//
+//  Created by qixiao on 2019/6/17.
+//  Copyright © 2019年 qixiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubTest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubTest1
+//
+//  Created by qixiao on 2019/6/17.
+//  Copyright © 2019年 qixiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
